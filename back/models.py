b0d0134665be40afcf6a830d8ece42d7ff0b8e17
DevLucxs <lucas.limoliveir@gmail.com>
2025-10-07 12:08:27 -0300

    Atualizando para versão com modais e filtros

--- back/models.py
+++ back/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Enum
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Enum, Float, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 import enum
 from database import Base
 
@@ -15,3 +15,18 @@ class User(Base):
     senha = Column(String(255), nullable=False)
     tipo = Column(Enum(UserType, native_enum=False), nullable=False)
     nome = Column(String(255), nullable=False)
+
+class Desempenho(Base):
+    __tablename__ = "desempenhos"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    atleta_id = Column(Integer, ForeignKey("users.id"))
+    treino = Column(String(255), nullable=False)
+    tempo = Column(Float)      # tempo em segundos
+    distancia = Column(Float)  # distância em metros
+    
+    atleta = relationship("User", back_populates="desempenhos")
+
+
+# Adicionar no User o relacionamento inverso
+User.desempenhos = relationship("Desempenho", back_populates="atleta")
