a0246ad656d31015ce4f8d7579598cacd758a251
DevLucxs <lucas.limoliveir@gmail.com>
2025-10-29 11:17:57 -0300

    Grafico de avaliar atleta

---
+++ back/front/static/js/apiClient.js
@@ -0,0 +1,11 @@
+﻿const apiClient = {
+    getAtletasDebug: async () => {
+        return await fetch('/api/alunos').then(res => res.json());
+    },
+    getEstatisticasAtleta: async (id) => {
+        return await fetch(`/api/alunos/${id}/estatisticas`).then(res => res.json());
+    },
+    getAvaliacoesAtleta: async (id) => {
+        return await fetch(`/api/alunos/${id}/feedbacks`).then(res => res.json());
+    }
+};
